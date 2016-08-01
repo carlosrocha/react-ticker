491be60ac289873f1e07c6dea2a7d7992aa0a69e
Carlos Rocha <carlos.rocha58@gmail.com>
2016-08-01 01:38:40 -0500

    Update

--- Ticker.js
+++ Ticker.js
@@ -1,46 +1,35 @@
 import React from 'react';
-import { TransitionMotion, spring } from 'react-motion';
+import {TransitionMotion, spring} from 'react-motion';
 
 export default function Ticker({item}) {
   return (
     <TransitionMotion
-      willEnter={key => ({
-        item,
-        opacity: spring(0),
-        y: spring(15),
+      willEnter={() => ({
+        opacity: 0,
+        y: 15,
       })}
-      willLeave={(key, {item}) => ({
-        item,
-        leaving: true,
+      willLeave={() => ({
         opacity: spring(0),
         y: spring(-15),
       })}
-      styles={{
-        [item.id]: {
-          item,
+      styles={[{
+        key: item.id,
+        data: item,
+        style: {
           opacity: spring(1),
           y: spring(0),
         },
-      }}>
-      {configs =>
-        <div style={{
-          position: 'relative',
-          border: '1px solid #bbb',
-          width: 300,
-          padding: '20px 0',
-          fontFamily: 'sans-serif',
-          fontSize: 30,
-          textAlign: 'center',
-        }}>
-          {Object.keys(configs).map(key =>
-            <div key={key}
+      }]}>
+      {(configs) =>
+        <div className="container">
+          {configs.map((config) =>
+            <div className="item"
+              key={config.key}
               style={{
-                width: '100%',
-                position: configs[key].leaving ? 'absolute' : 'static',
-                opacity: configs[key].opacity,
-                transform: `translateY(${configs[key].y}px)`,
+                opacity: config.style.opacity,
+                transform: `translateY(${config.style.y}px)`,
               }}>
-              {configs[key].item.text}
+              {config.data.text}
             </div>
           )}
         </div>
